2376eaba0c4efb65ad31549256f447ac4444daff
Mladen Čikara <mladen.cikara@gmail.com>
2018-07-08 16:12:58 +0200

    Initial import

---
+++ src/main/java/com/mladen/cikara/querydsl/QuerydslLearningTestApplication.java
@@ -0,0 +1,12 @@
+package com.mladen.cikara.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslLearningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslLearningTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mladen/cikara/querydsl/QuerydslLearningTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mladen.cikara.querydsl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuerydslLearningTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
